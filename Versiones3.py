f5416a7fafb53d635153d394735fe25e584f6e52
Camila Lopez <mclopez09@javerianacali.edu.co>
2019-04-19 14:52:45 -0500

    VersionesTres

--- Versiones3.py
+++ Versiones3.py
@@ -1,3 +1,3 @@
 print("Soy david y este es un nuevo commit")
 print("Soy david y este es un segundo commit")
-print("Soy david y este es un tercer commit")
+print("Soy david y este es un tercer commit"
